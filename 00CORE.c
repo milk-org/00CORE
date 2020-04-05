046f8a7ea3ed0178f54659a5bacd4cf9bed193b8
oguyon <oliv.guyon@gmail.com>
2020-04-05 12:59:10 -1000

    cleaner module info

--- 00CORE.c
+++ 00CORE.c
@@ -1,7 +1,7 @@
 /**
  * @file    00CORE.c
  * @brief   core functions
- * 
+ *
  * Useful functions frequently used by modules
  *
  *
@@ -22,7 +22,7 @@
 // if set to "", then calls use <funcname>
 #define MODULE_SHORTNAME_DEFAULT ""
 
-// Module short description 
+// Module short description
 #define MODULE_DESCRIPTION       "Core functions"
 
 // Application to which module belongs
@@ -41,7 +41,7 @@
 /* =============================================================================================== */
 /* =============================================================================================== */
 
-#include <fitsio.h> 
+#include <fitsio.h>
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
@@ -58,13 +58,16 @@
 #include <mach/mach_time.h>
 #define CLOCK_REALTIME 0
 #define CLOCK_MONOTONIC 0
-int clock_gettime(int clk_id, struct mach_timespec *t) {
+int clock_gettime(int clk_id, struct mach_timespec *t)
+{
     mach_timebase_info_data_t timebase;
     mach_timebase_info(&timebase);
     uint64_t time;
     time = mach_absolute_time();
-    double nseconds = ((double)time * (double)timebase.numer)/((double)timebase.denom);
-    double seconds = ((double)time * (double)timebase.numer)/((double)timebase.denom * 1e9);
+    double nseconds = ((double)time * (double)timebase.numer) / ((
+                          double)timebase.denom);
+    double seconds = ((double)time * (double)timebase.numer) / ((
+                         double)timebase.denom * 1e9);
     t->tv_sec = seconds;
     t->tv_nsec = nseconds;
     return 0;
@@ -106,33 +109,24 @@ static int INITSTATUS_00CORE = 0;
 /* =============================================================================================== */
 /** @name Module initialization */
 
+// for internal use
+static errno_t init_module_CLI()
+{
+    return RETURN_SUCCESS;
+}
+
+// for external use
+// may perform additional initialization
+errno_t init_00CORE()
+{
+    return init_module_CLI();
+}
 
 
 // Module initialization macro in CLIcore.h
 //
 INIT_MODULE_LIB();
 
-
-/*
-void __attribute__ ((constructor)) libinit_00CORE()
-{
-	if(INITSTATUS_00CORE == 0)
-	{
-		init_00CORE();
-		RegisterModule(__FILE__, "milk", "Core functions");
-		INITSTATUS_00CORE = 1;	
-	}
-}
-
-
-
-
-int init_00CORE()
-{
-    return 0;
-}
-
-*/
 
 
 
@@ -148,14 +142,14 @@ int init_00CORE()
 
 /**
  * ## Purpose
- * 
+ *
  * Print red string to stdout
- * 
+ *
  * ## Arguments
- * 
+ *
  * @param[in]
  * string	CHAR*
- */ 
+ */
 int printRED(char *string)
 {
     printf("%c[%d;%dm %s %c[%d;m\n", (char) 27, 1, 31, string, (char) 27, 0);
@@ -190,7 +184,7 @@ int printRED(char *string)
  * warnmessage		CHAR*
  * 			warning message to be printed
  *
- * 
+ *
  *  Color codes for printf
     	30	Black
     	31	Red
@@ -200,10 +194,10 @@ int printRED(char *string)
     	35	Magenta
     	36	Cyan
     	37	White
-    
+
  */
 
- errno_t printWARNING(
+errno_t printWARNING(
     const char *restrict file,
     const char *restrict func,
     int                  line,
@@ -216,14 +210,19 @@ int printRED(char *string)
     if(C_ERRNO != 0)
     {
         char buff[256];
-        if( strerror_r( errno, buff, 256 ) == 0 ) {
-            fprintf(stderr,"C Error: %s\n", buff );
+        if(strerror_r(errno, buff, 256) == 0)
+        {
+            fprintf(stderr, "C Error: %s\n", buff);
         }
         else
-            fprintf(stderr,"Unknown C Error\n");
+        {
+            fprintf(stderr, "Unknown C Error\n");
+        }
     }
     else
-        fprintf(stderr,"No C error (errno = 0)\n");
+    {
+        fprintf(stderr, "No C error (errno = 0)\n");
+    }
 
     fprintf(stderr,
             "%c[%d;%dm %s  %c[%d;m\n",
@@ -239,50 +238,57 @@ int printRED(char *string)
 
 /**
  * ## Purpose
- * 
+ *
  * Print error string
- * 
+ *
  * ## Arguments
- * 
+ *
  * @param[in]
  * file		CHAR*
  * 			file name from which error is issued
- * 
+ *
  * @param[in]
  * func		CHAR*
  * 			function name from which error is issued
- * 
+ *
  * @param[in]
  * line		int
  * 			line number from which error is issued
- * 
+ *
  * @param[in]
  * warnmessage		CHAR*
  * 			error message to be printed
- *  
- */ 
+ *
+ */
 
- errno_t printERROR(
+errno_t printERROR(
     const char *restrict file,
     const char *restrict func,
     int                  line,
     const char *restrict errmessage
 )
 {
-    fprintf(stderr, "%c[%d;%dm ERROR [ %s:%d: %s ]  %c[%d;m\n", (char) 27, 1, 31, file, line, func, (char) 27, 0);
+    fprintf(stderr, "%c[%d;%dm ERROR [ %s:%d: %s ]  %c[%d;m\n", (char) 27, 1, 31,
+            file, line, func, (char) 27, 0);
     if(C_ERRNO != 0)
     {
         char buff[256];
-        if( strerror_r( errno, buff, 256 ) == 0 ) {
-            fprintf(stderr,"C Error: %s\n", buff );
+        if(strerror_r(errno, buff, 256) == 0)
+        {
+            fprintf(stderr, "C Error: %s\n", buff);
         }
         else
-            fprintf(stderr,"Unknown C Error\n");
+        {
+            fprintf(stderr, "Unknown C Error\n");
+        }
     }
     else
-        fprintf(stderr,"No C error (errno = 0)\n");
+    {
+        fprintf(stderr, "No C error (errno = 0)\n");
+    }
 
-    fprintf(stderr,"%c[%d;%dm %s  %c[%d;m\n", (char) 27, 1, 31, errmessage, (char) 27, 0);
+    fprintf(stderr, "%c[%d;%dm %s  %c[%d;m\n", (char) 27, 1, 31, errmessage,
+            (char) 27, 0);
 
     C_ERRNO = 0;
 
@@ -299,18 +305,18 @@ int printRED(char *string)
 
 /**
  * ## Purpose
- * 
+ *
  * Log function call (for testing / debugging only).
  *
  * Function calls are logged if to file .FileName.funccalls.log
- * 
- * Variable AOLOOPCONTROL_logfunc_level keeps track of function depth: \n 
- * it is incremented when entering a function \n 
+ *
+ * Variable AOLOOPCONTROL_logfunc_level keeps track of function depth: \n
+ * it is incremented when entering a function \n
  * decremented when exiting a function
- * 
+ *
  * Variable AOLOOPCONTROL_logfunc_level_max sets the max depth of logging
- * 
- * 
+ *
+ *
  * At the beginning of each function, insert this code:
  * @code
  * #ifdef TEST
@@ -323,39 +329,39 @@ int printRED(char *string)
  * CORE_logFunctionCall( logfunc_level, logfunc_level_max, 1, __FILE__, __func__, __LINE__, "");
  * #endif
  * @endcode
- * 
- * 
+ *
+ *
  * ## Arguments
- * 
- * @param[in] 
+ *
+ * @param[in]
  * funclevel		INT
  * 					Function level (0: top level, always log)
- * 
- * @param[in] 
+ *
+ * @param[in]
  * loglevel			INT
  * 					Log level: log all function with level =< loglevel
- * 
+ *
  * logfuncMODE		INT
  * 					Log mode, 0:entering function, 1:exiting function
- * 
- * @param[in] 
+ *
+ * @param[in]
  * FileName			char*
  * 					Name of source file, usually __FILE__ so that preprocessor fills this parameter.
- * 
- * @param[in] 
+ *
+ * @param[in]
  * FunctionName		char*
  * 					Name of function, usually __FUNCTION__ so that preprocessor fills this parameter.
- * 
+ *
  * @param[in]
  * line				char*
  * 					Line in cource code, usually __LINE__ so that preprocessor fills this parameter.
- * 
+ *
  * @param[in]
  * comments			char*
  * 					comment string
  *
  * @return void
- * 
+ *
  * @note Carefully set depth value to avoid large output file.
  * @warning May slow down code. Only use for debugging. Output file may grow very quickly.
  */
@@ -364,7 +370,7 @@ void CORE_logFunctionCall(
     const int funclevel,
     const int loglevel,
     const int logfuncMODE,
-    __attribute__ ((unused)) const char *FileName,
+    __attribute__((unused)) const char *FileName,
     const char *FunctionName,
     const long line,
     char *comments
@@ -377,14 +383,18 @@ void CORE_logFunctionCall(
 
     modechar = '?';
 
-    if(logfuncMODE==0) {
+    if(logfuncMODE == 0)
+    {
         modechar = '>';
     }
-    else if(logfuncMODE==1) {
+    else if(logfuncMODE == 1)
+    {
         modechar = '<';
     }
     else
+    {
         modechar = '?';
+    }
 
     if(funclevel <= loglevel)
     {
@@ -425,23 +435,23 @@ void CORE_logFunctionCall(
 
 /**
  * ## Purpose
- * 
+ *
  * Set default precision (single or double)
- * 
+ *
  * ## Arguments
- * 
+ *
  * @param[in]
  * vp		int
  * 			precision mode
- * 
- * 
+ *
+ *
  * @param[in]
  * warnmessage		CHAR*
  * 			error message to be printed
  * -		0: single precision
  * -		1: double precision
- * 
- */ 
+ *
+ */
 int set_precision(int vp)
 {
     if(vp == 0)
@@ -456,7 +466,7 @@ int set_precision(int vp)
     }
     else
     {
-        printf("Requested precision value (%d) not a valid choice\n",vp);
+        printf("Requested precision value (%d) not a valid choice\n", vp);
     }
 
     return(0);
@@ -472,9 +482,9 @@ int CLIinfoPrint()
     printf("CONFIGDIR = %s\n", data.configdir);
     printf("SOURCEDIR = %s\n", data.sourcedir);
     printf("Memory usage      : %ld Mb  (%ld images)\n",
-           ((long) (compute_image_memory(data)/1024/1024)),
-           compute_nb_image(data)  );
-    printf("Default precision : %d (0: single, 1: double)\n",data.precision);
+           ((long)(compute_image_memory(data) / 1024 / 1024)),
+           compute_nb_image(data));
+    printf("Default precision : %d (0: single, 1: double)\n", data.precision);
 
     return(0);
 }
@@ -492,10 +502,15 @@ int CLIWritePid()
     pid = getpid();
     n = snprintf(command, 200, "touch CLI_%d", pid);
     if(n >= 200)
-        printERROR(__FILE__,__func__,__LINE__,"Attempted to write string buffer with too many characters");
+    {
+        printERROR(__FILE__, __func__, __LINE__,
+                   "Attempted to write string buffer with too many characters");
+    }
 
     if(system(command) != 0)
-        printERROR(__FILE__,__func__,__LINE__, "system() returns non-zero value");
+    {
+        printERROR(__FILE__, __func__, __LINE__, "system() returns non-zero value");
+    }
 
     return(0);
 }
@@ -508,12 +523,15 @@ struct timespec timespec_diff(struct timespec start, struct timespec end)
 {
     struct timespec temp;
 
-    if ((end.tv_nsec-start.tv_nsec)<0) {
-        temp.tv_sec = end.tv_sec-start.tv_sec-1;
-        temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
-    } else {
-        temp.tv_sec = end.tv_sec-start.tv_sec;
-        temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+    if((end.tv_nsec - start.tv_nsec) < 0)
+    {
+        temp.tv_sec = end.tv_sec - start.tv_sec - 1;
+        temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
+    }
+    else
+    {
+        temp.tv_sec = end.tv_sec - start.tv_sec;
+        temp.tv_nsec = end.tv_nsec - start.tv_nsec;
     }
     return temp;
 }
@@ -526,16 +544,19 @@ double timespec_diff_double(struct timespec start, struct timespec end)
     struct timespec temp;
     double val;
 
-    if ((end.tv_nsec-start.tv_nsec)<0) {
-        temp.tv_sec = end.tv_sec-start.tv_sec-1;
-        temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
-    } else {
-        temp.tv_sec = end.tv_sec-start.tv_sec;
-        temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+    if((end.tv_nsec - start.tv_nsec) < 0)
+    {
+        temp.tv_sec = end.tv_sec - start.tv_sec - 1;
+        temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
+    }
+    else
+    {
+        temp.tv_sec = end.tv_sec - start.tv_sec;
+        temp.tv_nsec = end.tv_nsec - start.tv_nsec;
     }
 
     val = temp.tv_sec;
-    val += 0.000000001*temp.tv_nsec;
+    val += 0.000000001 * temp.tv_nsec;
 
     return val;
 }
@@ -544,10 +565,10 @@ double timespec_diff_double(struct timespec start, struct timespec end)
 
 
 
-int file_exist (char *filename)
+int file_exist(char *filename)
 {
     struct stat   buffer;
-    return (stat (filename, &buffer) == 0);
+    return (stat(filename, &buffer) == 0);
 }
 
 
